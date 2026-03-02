2740fe38f42af24300c0aceb650000ea63da4750
Vlada024 <91543583+Vlada024@users.noreply.github.com>
2026-03-02 15:45:38 -0500

    Refactor bronze.load_bronze procedure to table definitions
    Removed the load_bronze procedure and added multiple table definitions for CRM and ERP data.

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -17,119 +17,58 @@ Usage Example:
 ===============================================================================
 */
 
-CREATE OR REPLACE PROCEDURE bronze.load_bronze()
-LANGUAGE plpgsql
-AS $$
-DECLARE
-    v_start_time        TIMESTAMP;
-    v_end_time          TIMESTAMP;
-    v_batch_start_time  TIMESTAMP;
-    v_batch_end_time    TIMESTAMP;
-BEGIN
-    v_batch_start_time := CLOCK_TIMESTAMP();
-    RAISE NOTICE '=================================';
-    RAISE NOTICE 'Loading Bronze Layer';
-    RAISE NOTICE '=================================';
-
-    RAISE NOTICE '---------------------------------';
-    RAISE NOTICE 'Loading CRM Tables';
-    RAISE NOTICE '---------------------------------';
-
-    v_start_time := CLOCK_TIMESTAMP();
-    RAISE NOTICE '>> Truncating Table bronze.crm_cust_info';
-    TRUNCATE TABLE bronze.crm_cust_info;
-
-    RAISE NOTICE '>> Getting the data into bronze.crm_cust_info';
-    COPY bronze.crm_cust_info
-    FROM '/tmp/datasets/source_crm/cust_info.csv'
-    WITH (FORMAT CSV, HEADER TRUE);
-    v_end_time := CLOCK_TIMESTAMP();
-    RAISE NOTICE '>> Load Duration: % seconds', EXTRACT(EPOCH FROM (v_end_time - v_start_time))::INT;
-    RAISE NOTICE '>> -------------';
+CREATE TABLE bronze.crm_cust_info (
+    cst_id              INT,
+    cst_key             VARCHAR(50),
+    cst_firstname       VARCHAR(50),
+    cst_lastname        VARCHAR(50),
+    cst_marital_status  VARCHAR(50),
+    cst_gndr            VARCHAR(50),
+    cst_create_date     DATE
+);
 
 
-    v_start_time := CLOCK_TIMESTAMP();
-    RAISE NOTICE '>> Truncating Table bronze.crm_prd_info';
-    TRUNCATE TABLE bronze.crm_prd_info;
+DROP TABLE IF EXISTS bronze.crm_prd_info;
+CREATE TABLE bronze.crm_prd_info (
+    prd_id       INT,
+    prd_key      VARCHAR(50),
+    prd_nm       VARCHAR(50),
+    prd_cost     INT,
+    prd_line     VARCHAR(50),
+    prd_start_dt TIMESTAMP,
+    prd_end_dt   TIMESTAMP
+);
 
+DROP TABLE IF EXISTS bronze.crm_sales_details;
+CREATE TABLE bronze.crm_sales_details (
+    sls_ord_num  VARCHAR(50),
+    sls_prd_key  VARCHAR(50),
+    sls_cust_id  INT,
+    sls_order_dt INT,
+    sls_ship_dt  INT,
+    sls_due_dt   INT,
+    sls_sales    INT,
+    sls_quantity INT,
+    sls_price    INT
+);
 
-    RAISE NOTICE '>> Getting the data into bronze.crm_prd_info';
-    COPY bronze.crm_prd_info
-    FROM '/tmp/datasets/source_crm/prd_info.csv'
-    WITH (FORMAT CSV, HEADER TRUE);
-    v_end_time := CLOCK_TIMESTAMP();
-    RAISE NOTICE '>> Load Duration: % seconds', EXTRACT(EPOCH FROM (v_end_time - v_start_time))::INT;
-    RAISE NOTICE '>> -------------';
+DROP TABLE IF EXISTS bronze.erp_loc_a101;
+CREATE TABLE bronze.erp_loc_a101 (
+    cid    VARCHAR(50),
+    cntry  VARCHAR(50)
+);
 
+DROP TABLE IF EXISTS bronze.erp_cust_az12;
+CREATE TABLE bronze.erp_cust_az12 (
+    cid    VARCHAR(50),
+    bdate  DATE,
+    gen    VARCHAR(50)
+);
 
-    v_start_time := CLOCK_TIMESTAMP();
-    RAISE NOTICE '>> Truncating Table bronze.crm_sales_info';
-    TRUNCATE TABLE bronze.crm_sales_details;
-
-    RAISE NOTICE '>> Getting the data into bronze.crm_sales_details';
-    COPY bronze.crm_sales_details
-    FROM '/tmp/datasets/source_crm/sales_details.csv'
-    WITH (FORMAT CSV, HEADER TRUE);
-    v_end_time := CLOCK_TIMESTAMP();
-    RAISE NOTICE '>> Load Duration: % seconds', EXTRACT(EPOCH FROM (v_end_time - v_start_time))::INT;
-    RAISE NOTICE '>> -------------';
-
-
-    RAISE NOTICE '---------------------------------';
-    RAISE NOTICE 'Loading ERP Tables';
-    RAISE NOTICE '---------------------------------';
-
-    v_start_time := CLOCK_TIMESTAMP();
-    RAISE NOTICE '>> Truncating Table bronze.erp_cust_az12';
-    TRUNCATE TABLE bronze.erp_cust_az12;
-
-    RAISE NOTICE '>> Getting the data into bronze.erp_cust_az12';
-    COPY bronze.erp_cust_az12
-    FROM '/tmp/datasets/source_erp/CUST_AZ12.csv'
-    WITH (FORMAT CSV, HEADER TRUE);
-    v_end_time := CLOCK_TIMESTAMP();
-    RAISE NOTICE '>> Load Duration: % seconds', EXTRACT(EPOCH FROM (v_end_time - v_start_time))::INT;
-    RAISE NOTICE '>> -------------';
-
-
-    v_start_time := CLOCK_TIMESTAMP();
-    RAISE NOTICE '>> Truncating Table bronze.erp_loc_a101';
-    TRUNCATE TABLE bronze.erp_loc_a101;
-
-    RAISE NOTICE '>> Getting the data into bronze.erp_loc_a101';
-    COPY bronze.erp_loc_a101
-    FROM '/tmp/datasets/source_erp/LOC_A101.csv'
-    WITH (FORMAT CSV, HEADER TRUE);
-    v_end_time := CLOCK_TIMESTAMP();
-    RAISE NOTICE '>> Load Duration: % seconds', EXTRACT(EPOCH FROM (v_end_time - v_start_time))::INT;
-    RAISE NOTICE '>> -------------';
-
-
-    v_start_time := CLOCK_TIMESTAMP();
-    RAISE NOTICE '>> Truncating Table bronze.erp_px_cat_g1v2';
-    TRUNCATE TABLE bronze.erp_px_cat_g1v2;
-
-    RAISE NOTICE '>> Getting the data into bronze.erp_px_cat_g1v2';
-    COPY bronze.erp_px_cat_g1v2
-    FROM '/tmp/datasets/source_erp/PX_CAT_G1V2.csv'
-    WITH (FORMAT CSV, HEADER TRUE);
-    v_end_time := CLOCK_TIMESTAMP();
-    RAISE NOTICE '>> Load Duration: % seconds', EXTRACT(EPOCH FROM (v_end_time - v_start_time))::INT;
-    RAISE NOTICE '>> -------------';
-
-
-    v_batch_end_time := CLOCK_TIMESTAMP();
-    RAISE NOTICE '==========================================';
-    RAISE NOTICE 'Loading Bronze Layer is Completed';
-    RAISE NOTICE '   - Total Load Duration: % seconds', EXTRACT(EPOCH FROM (v_batch_end_time - v_batch_start_time))::INT;
-    RAISE NOTICE '==========================================';
-
-
-    EXCEPTION WHEN OTHERS THEN
-    RAISE NOTICE '==========================================';
-    RAISE NOTICE 'ERROR OCCURED DURING LOADING BRONZE LAYER';
-    RAISE NOTICE 'Error Message: %', SQLERRM;
-    RAISE NOTICE 'Error Code: %', SQLSTATE;
-    RAISE NOTICE '==========================================';
-END;
-$$;
+DROP TABLE IF EXISTS bronze.erp_px_cat_g1v2;
+CREATE TABLE bronze.erp_px_cat_g1v2 (
+    id           VARCHAR(50),
+    cat          VARCHAR(50),
+    subcat       VARCHAR(50),
+    maintenance  VARCHAR(50)
+);
